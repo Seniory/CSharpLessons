43bb4eb026861649e599e249400524245add5ea5
Mohammad <Mohammad@MOHAMMAD-VAIO>
2018-04-12 11:29:14 +0300

    Add .gitignore and .gitattributes.

---
+++ MyVaribles/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVaribles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            int myvalue = 10;
+            Console.WriteLine("{0}", myvalue);
+            Console.ReadKey();
+        }
+    }
+}
